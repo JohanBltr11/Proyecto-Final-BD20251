c94ed033efd3e9fa5c44142c2a55df5cda8733f5
Johan Sebastián Beltrán Merchán <sebas08beltran@gmail.com>
2025-07-09 18:34:27 -0500

    clase conexión bd

---
+++ conexion/ConexionBD.java
@@ -0,0 +1,22 @@
+package conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionBD {
+    public static ConexionBD instancia=null;
+    String url= "Juan Diego y Carlos se aman en secreto";
+    
+    private ConexionBD() {
+	}
+
+	public static ConexionBD getInstancia() {
+		if (instancia == null) {
+			instancia = new ConexionBD();
+		}
+		return instancia;
+	}
+    
+
+}
